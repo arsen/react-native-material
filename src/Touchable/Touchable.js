26b107dbcaa2595d30c96926dba30e988af90d3b
Arsen Ghazaryan <arsen.ghazaryan@gmail.com>
2016-12-22 14:42:33 -0800

    calc native ui layout for touchable container

--- src/Touchable/Touchable.js
+++ src/Touchable/Touchable.js
@@ -3,16 +3,25 @@ import {
   View,
   Animated,
   TouchableWithoutFeedback,
-  PanResponder
+  PanResponder,
+  findNodeHandle,
+  NativeModules,
 } from 'react-native';
+
+const UIManager = NativeModules.UIManager;
 
 import styles from './Touchable.styles';
 
 export default class Touchable extends Component {
 
   static defaultProps = {
+    onPress: () => { },
+    onLongPress: () => { },
+    onPressIn: () => { },
+    onPressOut: () => { },
     overlayColor: 'rgba(0,0,0, 0.02)',
     rippleColor: 'rgba(0,0,0, 0.04)',
+    padding: {},
   }
 
   constructor(props) {
@@ -20,7 +29,7 @@ export default class Touchable extends Component {
 
     this.state = {
       overlayOpacity: new Animated.Value(0),
-      rippleScale: new Animated.Value(1),
+      rippleScale: new Animated.Value(0),
       rippleOpacity: new Animated.Value(1),
       layout: { width: 0, height: 0 },
       touchPosition: {
@@ -30,6 +39,9 @@ export default class Touchable extends Component {
     };
     this._panResponder = {};
     this._panResponderDisabled = {};
+    this.rippleSize = 0;
+    this.longPressTimeout = null;
+    this.layoutChanged = false;
   }
 
   componentWillMount() {
@@ -45,31 +57,50 @@ export default class Touchable extends Component {
       onMoveShouldSetPanResponderCapture: (evt, gestureState) => false,
       onStartShouldSetPanResponder: (evt, gestureState) => true,
       onStartShouldSetPanResponderCapture: (evt, gestureState) => true,
-      // onPanResponderTerminationRequest: (evt, gestureState) => true,
+
       onPanResponderGrant: (evt, gestureState) => {
         let touchPositionRelative = {
-          x: gestureState.x0 - this.state.layout.x,
-          y: gestureState.y0 - this.state.layout.y
+          x: gestureState.x0 - this.state.layout.pageX,
+          y: gestureState.y0 - this.state.layout.pageY
         }
         this.onTouchStart(touchPositionRelative);
+        this.props.onPressIn(evt, gestureState);
 
+        this.longPressTimeout = setTimeout(() => {
+          this.props.onLongPress();
+        }, 700);
       },
       onPanResponderRelease: (evt, gestureState) => {
+        clearTimeout(this.longPressTimeout);
         this.onTouchEnd();
+        this.props.onPressOut(evt, gestureState);
+        this.props.onPress();
       },
       onPanResponderTerminate: (evt, gestureState) => {
+        clearTimeout(this.longPressTimeout);
         this.onTouchEnd();
+        this.props.onPressOut(evt, gestureState);
       },
     });
   }
 
   onLayout(event) {
-    this.setState({ layout: event.nativeEvent.layout });
+    this.layoutChanged = true;
+    this.component && this.component.measure && this.component.measure((x, y, width, height, pageX, pageY) => {
+      this.setState({
+        layout: {
+          x, y, width, height, pageX, pageY
+        }
+      });
+    });
   }
 
+  onRef(component) {
+    this.component = component;
+  }
 
   onTouchStart(pos) {
-    this.state.rippleScale.setValue(1);
+    this.state.rippleScale.setValue(0);
     this.state.rippleOpacity.setValue(1);
     this.setState({
       touchPosition: pos
@@ -81,25 +112,25 @@ export default class Touchable extends Component {
     }).start();
 
 
-    let maxSize = Math.max(this.state.layout.width, this.state.layout.height) * 2;
-    let maxScale = Math.round(maxSize / 2);
+
+    this.rippleSize = parseInt(
+      2 * Math.sqrt(Math.pow(this.state.layout.width, 2) +
+        Math.pow(this.state.layout.height, 2))
+    );
 
     Animated.timing(this.state.rippleScale, {
-      toValue: maxScale,
+      toValue: this.rippleSize,
       duration: 2000
     }).start();
   }
 
   onTouchEnd() {
-    let maxSize = Math.max(this.state.layout.width, this.state.layout.height) * 2;
-    let maxScale = Math.round(maxSize / 2);
-
     Animated.timing(this.state.rippleScale, {
-      toValue: maxScale,
+      toValue: this.rippleSize,
     }).start();
 
     Animated.sequence([
-      Animated.delay(100),
+      Animated.delay(120),
       Animated.parallel([
         Animated.timing(this.state.overlayOpacity, {
           toValue: 0,
@@ -124,18 +155,31 @@ export default class Touchable extends Component {
       opacity: this.state.rippleOpacity,
       top: this.state.touchPosition.y,
       left: this.state.touchPosition.x,
-      transform: [
-        { scale: this.state.rippleScale },
-      ],
-      backgroundColor: this.props.rippleColor
+      backgroundColor: this.props.rippleColor,
+      borderRadius: 2000,
+      width: this.state.rippleScale,
+      height: this.state.rippleScale,
+      marginLeft: this.state.rippleScale.interpolate({
+        inputRange: [0, this.rippleSize],
+        outputRange: [0, -1 * (this.rippleSize / 2)]
+      }),
+      marginTop: this.state.rippleScale.interpolate({
+        inputRange: [0, this.rippleSize],
+        outputRange: [0, -1 * (this.rippleSize / 2)]
+      }),
     }
 
     return (
       <View
+        ref={(component) => {
+          this.onRef(component);
+        } }
         style={[styles.container, this.props.style]}
         onLayout={this.onLayout.bind(this)}
         {...this._panResponder.panHandlers} >
-        {this.props.children}
+        <View {...this._panResponderDisabled.panHandlers}>
+          {this.props.children}
+        </View>
         <Animated.View style={[styles.overlay, overlayStyles]}></Animated.View>
         <Animated.View style={[styles.ripple, rippleStyles]}></Animated.View>
       </View>
